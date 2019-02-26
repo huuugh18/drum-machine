9ece24501abc0f9ee89c498e3cd220e4330dab10
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-02-26 11:42:21 -0700

    finish drum machine to pass tests add background image

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 
+const drumElements = [
+  {id:'Splash', actionKey:'Q', source:'https://audio-previews.elements.envatousercontent.com/files/135160778/preview.mp3'},
+  {id:'Parrot', actionKey:'W', source:'https://audio-previews.elements.envatousercontent.com/files/148020959/preview.mp3'},
+  {id:'Bird', actionKey:'E', source:'https://audio-previews.elements.envatousercontent.com/files/146374366/preview.mp3'},
+  {id:'Horse', actionKey:'A', source:'https://audio-previews.elements.envatousercontent.com/files/136172320/preview.mp3'},
+  {id:'Dog', actionKey:'S', source:'https://audio-previews.elements.envatousercontent.com/files/25472138/preview.mp3'},
+  {id:'Thunder', actionKey:'D', source:'https://audio-previews.elements.envatousercontent.com/files/140207732/preview.mp3'},
+  {id:'Raven', actionKey:'Z', source:'https://audio-previews.elements.envatousercontent.com/files/133547872/preview.mp3'},
+  {id:'Sheep', actionKey:'X', source:'https://audio-previews.elements.envatousercontent.com/files/133976703/preview.mp3'},
+  {id:'Cow', actionKey:'C', source:'https://audio-previews.elements.envatousercontent.com/files/79938040/preview.mp3'}
+]
 
 class App extends Component {
   constructor(){
     super()
     this.state={
-        displaySound:''
+        displaySound:'Play Something!'
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.handleDisplay = this.handleDisplay.bind(this)
   }
-  handleClick = soundType => {
-    console.log(soundType)
-    this.setState({displaySound:soundType})
+  handleDisplay = newSound => {
+    this.setState({ displaySound:newSound })
   }
-  // handleClick (soundType) {
-  //   this.setState((state) => ({
-  //     displaySound: soundType
-  //   }))
-  // }
+
   render() {
     return (
       <div className="App" id="drum-machine">
-        <div id="display">
-        </div>
-        <div id="drum-components">
-          <DrumPadElement sound={'elephant'} actionKey={'Q'} onClickDrum={this.handleClick}/>
-          <DrumPadElement sound={'trumpet'} actionKey={'W'} onClickDrum={this.handleClick}/>
+        <Paper id="display-paper">
+          <Typography variant="h3" id="display">
+            {this.state.displaySound}
+          </Typography>
+        </Paper>
+        <div id="drum-kit">
+          {
+            drumElements.map(i => (
+              <DrumPadElement
+                key={i.id} 
+                sound={i.id} 
+                actionKey={i.actionKey} 
+                source={i.source} 
+                handleDisplay={this.handleDisplay}
+              />
+            ))
+          }
+
         </div>
       </div>
     );
@@ -41,24 +57,43 @@ class App extends Component {
 }
 
 class DrumPadElement extends Component {
-  // constructor(props){
-  //   super(props)
-
-  // }
-  handleClick = () => {
-    this.props.onClickDrum(this.props.sound)
+  componentDidMount () {
+    document.addEventListener('keydown', this.handleKeyDown)
+    window.focus()
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+  handleKeyDown = e => {
+    if(e.keyCode === this.props.actionKey.charCodeAt()) {
+      this.audio.play()
+      this.audio.currentTime = 0
+      this.props.handleDisplay(this.props.sound)
+    }
+  }
+  handleDrumClick = () => {
+    this.audio.play()
+    this.audio.currentTime = 0
+    this.props.handleDisplay(this.props.sound)
   }
   render(){
     return(
-      <div className="drum-pad" id={this.props.sound}>
         <Button 
-          onClick={this.handleClick}
-          color="primary"
-          variant="contained"
+          className='drum-pad'
+          id={this.props.sound}
+          onClick={this.handleDrumClick}
+          color='primary'
+          variant='contained'
+          size='large'
         >
           {this.props.actionKey}
+          <audio 
+            className={'clip'}
+            src={this.props.source} 
+            id={this.props.actionKey}
+            ref={ref => this.audio = ref}>
+          </audio>
         </Button>
-      </div>
     )
   }
 }
